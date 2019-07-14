1b1fc9d7cc21ae96e42e0a196b8156b3a9b55ff0
samuil <samuil.ivanovbg@gmail.com>
2019-07-14 21:24:56 +0300

    Adding Vue to the project

---
+++ assets/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- Your HTML tags -->
+    <h2 class="center"> Ahoy! </h2>
+</template>
+
+<script>
+    console.log("Loader");
+</script>
+
+<style>
+    .center {
+        text-align: center;
+    }
+</style>
